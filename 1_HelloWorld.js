e088a50f3e9f1cf6ff9802f468890bade026eb38
manhattan404 <76757456+manhattan404@users.noreply.github.com>
2021-07-26 12:35:57 -0400

    Create 1_HelloWorld.js

---
+++ 1_HelloWorld.js
@@ -0,0 +1,9 @@
+alert("Hello World") // alert is a JS function
+
+let js = 'amazing' // assigning a string to a variable 
+if (js === 'amazing') alert('Javascript is fun') // conditional
+
+js = 'boring' // this will return 'undefined' because it's 'boring' now 
+
+// You can also do simple math operations on Chrome console
+9 + 10 // press enter 
